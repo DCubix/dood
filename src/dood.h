61e7e814201e803f7c009f176b1a8e574c9f7cc3
twister <diego95lopes@live.com>
2020-03-09 13:11:52 -0400

    Clipping, lines, rectangles and canvas to canvas

--- src/dood.h
+++ src/dood.h
@@ -6,6 +6,7 @@
 #define _dood_comp_cast(x) ((uint8_t)((x) * 255.0f) & 0xFF)
 #define _dood_lerp(a, b, t) ((1.0f - (t)) * (a) + (b) * (t))
 #define _dood_clamp(a, mi, ma) ((a) >= (ma) ? (ma) : (a) <= (mi) ? (mi) : (a))
+#define _dood_remap(v, fmi, foma, tmi, tma) ((tmi) + (((v) - (fmi)) / ((foma) - (fmi))) * ((tma) - (tmi)))
 
 enum BlendMode {
 	BM_OPAQUE = 0,
@@ -25,15 +26,31 @@ extern Canvas dood_canvas_new(int width, int height);
 extern void dood_canvas_free(Canvas canvas);
 
 extern void dood_canvas_clear(Canvas canvas);
+extern void dood_canvas_put(Canvas canvas, int x, int y, float r, float g, float b, float a);
 extern void dood_canvas_draw_point(Canvas canvas, int x, int y);
+extern void dood_canvas_draw_line(Canvas canvas, int x1, int y1, int x2, int y2);
+extern void dood_canvas_draw_rect(Canvas canvas, int x, int y, int w, int h);
+extern void dood_canvas_fill_rect(Canvas canvas, int x, int y, int w, int h);
+
+extern void dood_canvas_draw_canvas(
+	Canvas dest, Canvas src,
+	int sx, int sy, int sw, int sh,
+	int dx, int dy, int dw, int dh
+);
 
 extern void dood_canvas_get(Canvas canvas, int x, int y, float* r, float* g, float* b, float* a);
 extern void dood_canvas_get_size(Canvas canvas, int* w, int* h);
 
 extern void dood_canvas_set_blend(Canvas canvas, int blendMode);
 extern void dood_canvas_set_draw_color(Canvas canvas, float r, float g, float b, float a);
+extern void dood_canvas_set_draw_color_gray(Canvas canvas, float v);
+
+extern void dood_canvas_set_clip(Canvas canvas, int x, int y, int w, int h);
+extern void dood_canvas_remove_clip(Canvas canvas);
 
 extern void dood_canvas_save_ppm(Canvas canvas, const char* outFile);
+
+extern uint8_t* dood_canvas_get_buffer(Canvas canvas, int drawBuffer);
 
 #ifdef DOOD_IMPLEMENTATION
 
@@ -42,10 +59,12 @@ extern void dood_canvas_save_ppm(Canvas canvas, const char* outFile);
 
 struct Canvas_t {
 	int width, height;
+
 	uint8_t* pixels;
+	int clip[4];
 
 	Color drawColor;
-	int blendMode;
+	int blendMode, drawBuffer;
 };
 
 Canvas dood_canvas_new(int width, int height) {
@@ -54,8 +73,36 @@ Canvas dood_canvas_new(int width, int height) {
 	cnv->height = height;
 	cnv->blendMode = BM_OPAQUE;
 	cnv->pixels = (uint8_t*) malloc(sizeof(uint8_t) * width * height * 4);
+	dood_canvas_remove_clip(cnv);
 	dood_canvas_set_draw_color(cnv, 0, 0, 0, 1.0f);
 	return cnv;
+}
+
+Canvas dood_canvas_load_ppm(const char* fileName) {
+	FILE* fp = fopen(fileName, "r");
+	if (fp) {
+		char header[3];
+		fscanf(fp, "%s", header);
+		if (header[1] != '3') {
+			fclose(fp);
+			return NULL;
+		}
+
+		int w, h, d;
+		fscanf(fp, "%d %d", &w, &h);
+		fscanf(fp, "%d", &d);
+
+		Canvas cnv = dood_canvas_new(w, h);
+		for (int i = 0; i < w * h; i++) {
+			int r, g, b;
+			fscanf(fp, "%d %d %d", &r, &g, &b);
+			dood_canvas_put(cnv, i % w, i / w, (float)r / 255.0f, (float)g / 255.0f, (float)b / 255.0f, 1.0f);
+		}
+		return cnv;
+
+		fclose(fp);
+	}
+	return NULL;
 }
 
 void dood_canvas_free(Canvas canvas) {
@@ -63,17 +110,9 @@ void dood_canvas_free(Canvas canvas) {
 	free(canvas);
 }
 
-void dood_canvas_draw_point(Canvas canvas, int x, int y) {
-	if (x < 0 || x >= canvas->width || y < 0 || y >= canvas->height) {
-		return;
-	}
-	int i = (x + y * canvas->width) * 4;
+void dood_canvas_put(Canvas canvas, int x, int y, float r, float g, float b, float a) {
+	if (x < canvas->clip[0] || x >= canvas->clip[2] || y < canvas->clip[1] || y >= canvas->clip[3]) return;
 
-	float r, g, b, a;
-	r = canvas->drawColor.r;
-	g = canvas->drawColor.g;
-	b = canvas->drawColor.b;
-	a = canvas->drawColor.a;
 	switch (canvas->blendMode) {
 		default: break;
 		case BM_MULTIPLY: {
@@ -106,10 +145,51 @@ void dood_canvas_draw_point(Canvas canvas, int x, int y) {
 	b = _dood_clamp(b, 0.0f, 1.0f);
 	a = _dood_clamp(a, 0.0f, 1.0f);
 
-	canvas->pixels[i + 0] = _dood_comp_cast(r);
-	canvas->pixels[i + 1] = _dood_comp_cast(g);
-	canvas->pixels[i + 2] = _dood_comp_cast(b);
-	canvas->pixels[i + 3] = _dood_comp_cast(a);
+	uint8_t* buf = canvas->pixels;
+	int i = (x + y * canvas->width) * 4;
+	buf[i + 0] = _dood_comp_cast(r);
+	buf[i + 1] = _dood_comp_cast(g);
+	buf[i + 2] = _dood_comp_cast(b);
+	buf[i + 3] = _dood_comp_cast(a);
+}
+
+void dood_canvas_draw_point(Canvas canvas, int x, int y) {
+	dood_canvas_put(canvas, x, y, canvas->drawColor.r, canvas->drawColor.g, canvas->drawColor.b, canvas->drawColor.a);
+}
+
+void dood_canvas_draw_line(Canvas canvas, int x1, int y1, int x2, int y2) {
+	int dx = abs(x2 - x1);
+	int sx = x1 < x2 ? 1 : -1;
+	int dy = -abs(y2 - y1);
+	int sy = y1 < y2 ? 1 : -1;
+	int err = dx + dy;
+	int e2 = 0;
+
+	int x = x1;
+	int y = y1;
+
+	while (1) {
+		dood_canvas_draw_point(canvas, x, y);
+		if (x == x2 && y == y2) break;
+		e2 = 2 * err;
+		if (e2 >= dy) { err += dy; x += sx; }
+		if (e2 <= dx) { err += dx; y += sy; }
+	}
+}
+
+void dood_canvas_draw_rect(Canvas canvas, int x, int y, int w, int h) {
+	dood_canvas_draw_line(canvas, x, y, x + w, y);
+	dood_canvas_draw_line(canvas, x + w, y, x + w, y + h);
+	dood_canvas_draw_line(canvas, x, y + h, x + w, y + h);
+	dood_canvas_draw_line(canvas, x, y, x, y + h);
+}
+
+void dood_canvas_fill_rect(Canvas canvas, int x, int y, int w, int h) {
+	for (int py = y; py < y + h; py++) {
+		for (int px = x; px < x + w; px++) {
+			dood_canvas_draw_point(canvas, px, py);
+		}
+	}
 }
 
 void dood_canvas_get(Canvas canvas, int x, int y, float* r, float* g, float* b, float* a) {
@@ -128,6 +208,10 @@ void dood_canvas_get_size(Canvas canvas, int* w, int* h) {
 	if (h) *h = canvas->height;
 }
 
+void dood_canvas_set_draw_buffer(Canvas canvas, int drawBuffer) {
+	canvas->drawBuffer = drawBuffer;
+}
+
 void dood_canvas_set_draw_color(Canvas canvas, float r, float g, float b, float a) {
 	canvas->drawColor.r = r;
 	canvas->drawColor.g = g;
@@ -135,8 +219,27 @@ void dood_canvas_set_draw_color(Canvas canvas, float r, float g, float b, float 
 	canvas->drawColor.a = a;
 }
 
+void dood_canvas_set_draw_color_gray(Canvas canvas, float v) {
+	dood_canvas_set_draw_color(canvas, v, v, v, 1.0f);
+}
+
 void dood_canvas_set_blend(Canvas canvas, int blendMode) {
 	canvas->blendMode = blendMode;
+}
+
+void dood_canvas_set_clip(Canvas canvas, int x, int y, int w, int h) {
+	if (x * y + (x+w) * (y+h) == 0) {
+		dood_canvas_remove_clip(canvas);
+		return;
+	}
+	canvas->clip[0] = x;
+	canvas->clip[1] = y;
+	canvas->clip[2] = x + w;
+	canvas->clip[3] = x + h;
+}
+
+void dood_canvas_remove_clip(Canvas canvas) {
+	dood_canvas_set_clip(canvas, 0, 0, canvas->width, canvas->height);
 }
 
 void dood_canvas_clear(Canvas canvas) {
@@ -145,6 +248,22 @@ void dood_canvas_clear(Canvas canvas) {
 		canvas->pixels[i + 1] = _dood_comp_cast(canvas->drawColor.g);
 		canvas->pixels[i + 2] = _dood_comp_cast(canvas->drawColor.b);
 		canvas->pixels[i + 3] = _dood_comp_cast(canvas->drawColor.a);
+	}
+}
+
+void dood_canvas_draw_canvas(
+	Canvas dest, Canvas src,
+	int sx, int sy, int sw, int sh,
+	int dx, int dy, int dw, int dh
+) {
+	for (int y = 0; y < dh; y++) {
+		for (int x = 0; x < dw; x++) {
+			int cx = (int) _dood_remap((float) x, 0.0f, (float)dw, 0.0f, (float)sw);
+			int cy = (int) _dood_remap((float) y, 0.0f, (float)dh, 0.0f, (float)sh);
+			float pr = 0.0f, pg = 0.0f, pb = 0.0f, pa = 0.0f;
+			dood_canvas_get(src, cx + sx, cy + sy, &pr, &pg, &pb, &pa);
+			dood_canvas_put(dest, x + dx, y + dy, pr, pg, pb, pa);
+		}
 	}
 }
 
--- src/main.c
+++ src/main.c
@@ -5,26 +5,16 @@
 #include "dood.h"
 
 int main(int argc, char** argv) {
+	Canvas prev = dood_canvas_load_ppm("out.ppm");
 	Canvas cnv = dood_canvas_new(256, 256);
 
+	dood_canvas_clear(cnv);
 
-	for (int y = 0; y < 256; y++) {
-		for (int x = 0; x < 256; x++) {
-			float fx = (float)x / 256.0f;
-			float fy = (float)y / 256.0f;
+	dood_canvas_draw_canvas(cnv, prev, 0, 0, 256, 256, 0, 0, 128, 256);
 
-			dood_canvas_set_blend(cnv, BM_OPAQUE);
-			dood_canvas_set_draw_color(cnv, fx, fy, 1.0f - (fx * fy), 1.0f);
-			dood_canvas_draw_point(cnv, x, y);
-
-			dood_canvas_set_blend(cnv, BM_ADDITIVE);
-			dood_canvas_set_draw_color(cnv, 1.0f, 0.0f, 0.0f, (sinf(((fx + fy) * 0.5f) * 16.0f) * 0.5f + 0.5f));
-			dood_canvas_draw_point(cnv, x, y);
-		}
-	}
-
-	dood_canvas_save_ppm(cnv, "out.ppm");
+	dood_canvas_save_ppm(cnv, "out2.ppm");
 
 	dood_canvas_free(cnv);
+	dood_canvas_free(prev);
 	return 0;
 }
